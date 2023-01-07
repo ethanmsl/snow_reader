4aa7e6983b83034d0f5a0a98b6b72aaf28a96aa1
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-06 17:40:34 -0800

    feat: read file into in-mem obj -- iterate over said obj

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// use file system
+
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+
+    // read in file
+    let contents =
+        fs::read_to_string("snow_files/sys_script_include.xml").expect("\nFile Read Error\n");
+
+    // iterate over in-memory clone of file
+    contents
+        .lines()
+        .take(20)
+        .for_each(|line| println!("{}", line));
+
+    // // set up a stream to read file from
+    // let mut stream = fs::File::open("snow_files/sys_script_include.xml").expect("\nFile Read Error\n");
 }
