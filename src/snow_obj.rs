96fc803bdb9d2a987a2cd6ecc7697f32d2a5b5c4
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 02:36:19 -0800

    feat: expanded deserialization struct (succesfully, re: long example)

--- src/snow_obj.rs
+++ src/snow_obj.rs
@@ -5,14 +5,31 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Unload {
-    sysevent_script_action: Option<Vec<OtherStuff>>,
-    sys_script_include: Option<Vec<OtherStuff>>,
+    sysevent_script_action: Option<Vec<ScriptContents>>,
+    sys_script_include: Option<Vec<ScriptContents>>,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct OtherStuff {
+pub struct ScriptContents {
+    access: Option<String>,
     active: Option<String>,
+    api_name: Option<String>,
+    client_callable: Option<String>,
+    description: Option<String>,
+    name: Option<String>,
+    script: Option<String>,
     event_name: Option<String>,
+    sys_class_name: Option<String>,
+    sys_created_by: Option<String>,
+    sys_created_on: Option<String>,
+    sys_customer_update: Option<String>,
+    sys_id: Option<String>,
+    sys_mod_count: Option<String>,
+    sys_name: Option<String>,
+    sys_packaged_display_value: Option<String>,
     sys_replace_on_upgrade: Option<String>,
-    sys_scope: Option<String>,
+    sys_scope_display_value: Option<String>,
+    sys_update_name: Option<String>,
+    sys_updated_by: Option<String>,
+    sys_updated_on: Option<String>,
 }
