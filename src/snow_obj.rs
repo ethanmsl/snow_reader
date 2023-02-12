887ca574833a1e5a43e52094026bb6dcb87e7cfa
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 03:17:50 -0800

    feat: shorter display of scritpcontents and (non-functioning) alternate struct

--- src/snow_obj.rs
+++ src/snow_obj.rs
@@ -10,25 +10,54 @@ pub struct Unload {
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct ScriptContents {
-    access: Option<String>,
-    active: Option<String>,
-    api_name: Option<String>,
-    client_callable: Option<String>,
-    description: Option<String>,
+    // access: Option<String>,
+    // active: Option<String>,
+    // api_name: Option<String>,
+    // client_callable: Option<String>,
+    // description: Option<String>,
     name: Option<String>,
-    script: Option<String>,
-    event_name: Option<String>,
-    sys_class_name: Option<String>,
-    sys_created_by: Option<String>,
-    sys_created_on: Option<String>,
-    sys_customer_update: Option<String>,
-    sys_id: Option<String>,
-    sys_mod_count: Option<String>,
-    sys_name: Option<String>,
-    sys_packaged_display_value: Option<String>,
-    sys_replace_on_upgrade: Option<String>,
-    sys_scope_display_value: Option<String>,
-    sys_update_name: Option<String>,
-    sys_updated_by: Option<String>,
-    sys_updated_on: Option<String>,
+    // script: Option<String>,
+    // event_name: Option<String>,
+    // sys_class_name: Option<String>,
+    // sys_created_by: Option<String>,
+    // sys_created_on: Option<String>,
+    // sys_customer_update: Option<String>,
+    // sys_id: Option<String>,
+    // sys_mod_count: Option<String>,
+    // sys_name: Option<String>,
+    // sys_packaged_display_value: Option<String>,
+    // sys_replace_on_upgrade: Option<String>,
+    // sys_scope_display_value: Option<String>,
+    // sys_update_name: Option<String>,
+    // sys_updated_by: Option<String>,
+    // sys_updated_on: Option<String>,
+}
+
+
+
+
+
+
+/////////////////////////////////////////
+// this set doesn't capture info, but seems like it should
+// the above doesn't seem like it should capture more than one script
+// but captures all their contents, but errantly as children on the same level
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Unload2 {
+    // sysevent_script_action: Option<Vec<ScriptContents>>,
+    #[serde(rename = "$value")]
+    scripts: Vec<Script>,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum Script {
+    SysScriptInclude(SysScriptInclude),
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct SysScriptInclude {
+    // sysevent_script_action: Option<Vec<ScriptContents>>,
+    sys_script_include: Option<ScriptContents>,
 }
