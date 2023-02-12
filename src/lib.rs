f931e208af8de776555a74b4803860112b8ba87a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 00:02:16 -0800

    refactor: moved the load and plain print to library

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
 //! Look -- I'm the `lib.rs`'s doc string! :)
 
+use std::fs;
+
 /// An example function
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+/// reads from a file and prints
+pub fn basic_read() {
+    // read in file
+    let contents =
+        fs::read_to_string("snow_files/sys_script_include.xml").expect("\nFile Read Error\n");
+
+    // iterate over in-memory clone of file
+    contents
+        .lines()
+        .take(40)
+        .for_each(|line| println!("{}", line));
+
+    // // set up a stream to read file from
+    // let mut stream = fs::File::open("snow_files/sys_script_include.xml").expect("\nFile Read Error\n");
 }
 
 #[cfg(test)]
