ce0ed738852f306b98d3b7b7cb66470fb6a216fc
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 00:12:08 -0800

    feat: added load-in functions for two example snow files, w/doc-com

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,44 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-/// reads from a file and prints
-pub fn basic_read() {
-    // read in file
+/// reads in a long xml-encoded SNOW file
+/// (**14_831 lines**)
+///
+/// # Conceptual Structure:
+/// - root
+///     - unload
+///         - sysevent_script_action (w/action = "INSERT_OR_UPDATE")
+///             - various
+///             - various
+///             - ...
+///     - unload
+///         - ...
+///     - ...
+pub fn snow_example_long() -> String {
     let contents =
         fs::read_to_string("snow_files/sys_script_include.xml").expect("\nFile Read Error\n");
+    contents
+}
+
+/// reads in a short xml-encoded SNOW file
+/// (**42 lines**)
+///
+/// # Conceptual Structure:
+/// - root
+///     - unload
+///         - sysevent_script_action (w/action = "INSERT_OR_UPDATE")
+///             - various
+///             - various
+///             - ...
+pub fn snow_example_short() -> String {
+    let contents =
+        fs::read_to_string("snow_files/sysevent_script_action.xml").expect("\nFile Read Error\n");
+    contents
+}
+
+/// reads from a file and prints
+pub fn basic_read() {
+    let contents = snow_example_long();
 
     // iterate over in-memory clone of file
     contents
