7964b9cd0ff0b838869dc16ee7eb0122a23c4139
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 03:28:52 -0800

    feat: working, repeateable, enum based parsing with `serde` extension

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,25 @@ use serde_xml_rs::from_str;
 use snow_read::snow_obj::Unload;
 
 fn main() {
-    // #[derive(Debug, Serialize, Deserialize, PartialEq)]
-    // struct PlateAppearance {
-    //     #[serde(rename = "$value")]
-    //     events: Vec<String>
-    // }
-
-    println!("--------------------------------");
-    let contents = snow_read::snow_example_art();
-    let doc = roxmltree::Document::parse(&contents).unwrap();
-    println!("roxmltree doc:\n\n{:?}", doc);
-
-    println!("--------------------------------");
     println!("--------------------------------");
     let contents = snow_read::snow_example_long();
     let item: Unload = from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
     // println!("deserd'd:\n\n{:?}", item.unwrap());
     dbg!(item);
+
+    println!("--------------------------------");
+    let contents = snow_read::snow_example_short();
+    let item: Unload = from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
+    // println!("deserd'd:\n\n{:?}", item.unwrap());
+    dbg!(item);
+
+    /////////////////////////////////////////////////////////
+    // a `roxmltree` based parsing
+    // What's **nice**: is that it gives all the info
+    // (whereas `xml_serde_rs` seems to toss out attribute info)
+
+    // println!("--------------------------------");
+    // let contents = snow_read::snow_example_art();
+    // let doc = roxmltree::Document::parse(&contents).unwrap();
+    // println!("roxmltree doc:\n\n{:?}", doc);
 }
