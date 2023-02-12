e5e658527afd7b92d43cd64023d4fc3e92c10d47
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 03:37:31 -0800

    feat: made structs' fields public (and uncommented-out)

--- src/snow_obj.rs
+++ src/snow_obj.rs
@@ -2,13 +2,16 @@
 
 use serde::{Deserialize, Serialize};
 
+/// the Parent(most) structure
+/// a series of script types
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Unload {
     // sysevent_script_action: Option<Vec<ScriptContents>>,
     #[serde(rename = "$value")]
-    scripts: Vec<Script>,
+    pub scripts: Vec<Script>,
 }
 
+/// the kinds of Scripts that can be found
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum Script {
@@ -16,27 +19,29 @@ pub enum Script {
     SyseventScriptAction(ScriptContents),
 }
 
+/// represents the content elements of an individual script
+/// Currently all set as "Option" -- as I'm uncertain regarding consistency
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct ScriptContents {
-    // access: Option<String>,
-    // active: Option<String>,
-    api_name: Option<String>,
-    // client_callable: Option<String>,
-    // description: Option<String>,
-    name: Option<String>,
-    // script: Option<String>,
-    // event_name: Option<String>,
-    // sys_class_name: Option<String>,
-    sys_created_by: Option<String>,
-    // sys_created_on: Option<String>,
-    // sys_customer_update: Option<String>,
-    // sys_id: Option<String>,
-    // sys_mod_count: Option<String>,
-    // sys_name: Option<String>,
-    // sys_packaged_display_value: Option<String>,
-    // sys_replace_on_upgrade: Option<String>,
-    // sys_scope_display_value: Option<String>,
-    // sys_update_name: Option<String>,
-    // sys_updated_by: Option<String>,
-    // sys_updated_on: Option<String>,
+    pub access: Option<String>,
+    pub active: Option<String>,
+    pub api_name: Option<String>,
+    pub client_callable: Option<String>,
+    pub description: Option<String>,
+    pub name: Option<String>,
+    pub script: Option<String>,
+    pub event_name: Option<String>,
+    pub sys_class_name: Option<String>,
+    pub sys_created_by: Option<String>,
+    pub sys_created_on: Option<String>,
+    pub sys_customer_update: Option<String>,
+    pub sys_id: Option<String>,
+    pub sys_mod_count: Option<String>,
+    pub sys_name: Option<String>,
+    pub sys_packaged_display_value: Option<String>,
+    pub sys_replace_on_upgrade: Option<String>,
+    pub sys_scope_display_value: Option<String>,
+    pub sys_update_name: Option<String>,
+    pub sys_updated_by: Option<String>,
+    pub sys_updated_on: Option<String>,
 }
