0c900b98ef831df3fb30989fff899968b787a978
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 04:14:19 -0800

    explr: accessing fields from deserialized structs

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@
 #![allow(clippy::uninlined_format_args)]
 
 use serde_xml_rs::from_str;
-use snow_read::snow_obj::Unload;
+use snow_read::snow_obj::{Script, Unload};
 
 fn main() {
     println!("--------------------------------");
     let contents = snow_read::snow_example_long();
-    let item: Unload = from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
+    let item_long: Unload = from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
     // println!("deserd'd:\n\n{:?}", item.unwrap());
-    dbg!(item);
+    // dbg!(item_long);
 
     println!("--------------------------------");
     let contents = snow_read::snow_example_short();
-    let item: Unload = from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
+    let item_short: Unload =
+        from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
     // println!("deserd'd:\n\n{:?}", item.unwrap());
-    dbg!(item);
+    let _item_short = dbg!(item_short);
+
+    println!("--------------------------------");
+    for elem in &item_long.scripts {
+        match elem {
+            Script::SysScriptInclude(x) => println!("\n{:?}", x.name),
+            Script::SyseventScriptAction(x) => println!("\n{:?}", x.name),
+        }
+    }
 }
