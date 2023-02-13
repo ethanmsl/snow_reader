05e646051122b2c1909bf1de20838c425d04f5ea
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-12 19:40:13 -0800

    style: minor change in output style

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,9 @@ fn main() {
             Script::SysScriptInclude(x) | Script::SyseventScriptAction(x) => x,
         };
         if search_contents.name == contents.name {
-            println!("found: {:?}", search_contents.name);
-
+            println!("\nATTENTION.");
+            println!("File difference detected in file:
+                                    --- {} ---\n", search_contents.name);
             let old = contents.script.as_ref().unwrap();
             let new = search_contents.script.as_ref().unwrap();
 
