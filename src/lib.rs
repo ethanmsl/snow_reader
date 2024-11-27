3a386ed53f13d701ef58d86178b3b75199f141ce
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-27 16:26:58 -0500

    wip: refactor & exploration

--- src/lib.rs
+++ src/lib.rs
@@ -24,49 +24,56 @@ const EXAMPLE_SCRIPT_INCLUDE: &str = "snow_files/sys_script_include.xml";
 pub fn snow_example_long<P>(path: Option<&P>) -> Result<String>
         where P: AsRef<Path>
 {
-        let string_path = match path {
+        Ok(match path {
                 Some(path) => fs::read_to_string(path)?,
                 None => fs::read_to_string(EXAMPLE_SCRIPT_INCLUDE)?,
-        };
-        Ok(string_path)
+        })
 }
 
-/// extracted & altered script-include to use to test search & diff'ing
-pub fn snow_example_search() -> String
-{
-        fs::read_to_string("snow_files/search_include.xml").expect("\nFile Read Error\n")
-}
+// /// extracted & altered script-include to use to test search & diff'ing
+// pub fn snow_example_search<P>(path: Option<&P>) -> Result<String>
+//         where P: AsRef<Path>
+// {
+//         Ok(match path {
+//                 Some(path) => fs::read_to_string(path)?,
+//                 None => fs::read_to_string(EXAMPLE_SEARCH_INCLUDE)?,
+//         })
+// }
 
-/// reads in a short xml-encoded SNOW file
-/// (uses diff't script type than `sys_script_include`)
-/// (**42 lines**)
-///
-/// # Conceptual Structure:
-/// - root
-///     - unload
-///         - sysevent_script_action (w/action = "INSERT_OR_UPDATE")
-///             - various
-///             - various
-///             - ...
-pub fn snow_example_short() -> String
-{
-        fs::read_to_string("snow_files/sysevent_script_action.xml").expect("\nFile Read Error\n")
-}
+// /// reads in a short xml-encoded SNOW file
+// /// (uses diff't script type than `sys_script_include`)
+// /// (**42 lines**)
+// ///
+// /// # Conceptual Structure:
+// /// - root
+// ///     - unload
+// ///         - sysevent_script_action (w/action = "INSERT_OR_UPDATE")
+// ///             - various
+// ///             - various
+// ///             - ...
+// pub fn snow_example_short<P>(path: Option<&P>) -> Result<String>
+//         where P: AsRef<Path>
+// {
+//         Ok(match path {
+//                 Some(path) => fs::read_to_string(path)?,
+//                 None => fs::read_to_string(EXAMPLE_SCRIPT_ACTION)?,
+//         })
+// }
 
-/// reads in an artificial (very short) xml-encoded SNOW file
-/// (**9 lines**)
-///
-/// # Conceptual Structure:
-/// - root
-///     - unload
-///         - sysevent_script_action (w/action = "INSERT_OR_UPDATE")
-///             - various
-///             - various
-///             - ...
-pub fn snow_example_art() -> String
-{
-        fs::read_to_string("snow_files/fake_simple.xml").expect("\nFile Read Error\n")
-}
+// /// reads in an artificial (very short) xml-encoded SNOW file
+// /// (**9 lines**)
+// ///
+// /// # Conceptual Structure:
+// /// - root
+// ///     - unload
+// ///         - sysevent_script_action (w/action = "INSERT_OR_UPDATE")
+// ///             - various
+// ///             - various
+// ///             - ...
+// pub fn snow_example_art() -> String
+// {
+//         fs::read_to_string("snow_files/fake_simple.xml").expect("\nFile Read Error\n")
+// }
 
 // // Error specification issue -- I need the internet to figure this out
 // /// reads from a (long) xml-encoded SNOW file and a section of raw lines to screen
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 //! `main.rs`
 
+mod error;
+
+use std::fs;
+
+use error::Result;
 use snow_read::{differ::diff_inline,
                 snow_serde::{Script, ScriptContents, Unload}};
 use struct_field_names_as_array::FieldNamesAsArray;
-mod error;
-use error::Result;
+use tracing::{debug, info, trace};
+use tracing_subscriber::EnvFilter;
+
+// long xml-encoded SNow file
+const SNOW_EXAMPLE_LONG_FILE: &str = "snow_files/sys_script_include.xml";
+// extracted & altered script-include to use to test search & diff'ing
+const SNOW_EXAMPLE_SEARCH_FILE: &str = "snow_files/search_include.xml";
+// short xml-encoded SNow file
+const _SNOW_EXAMPLE_SHORT_FILE: &str = "snow_files/sysevent_script_action.xml";
+// artificial, very short xml-encoded SNow file
+const _SNOW_FAKE_SIMPLE_FILE: &str = "snow_files/fake_simple.xml";
 
 fn main() -> Result<()>
 {
+        tracing_subscriber::fmt().pretty().with_env_filter(EnvFilter::from_default_env()).init();
+        info!("Starting up...");
+        debug!("hi....");
         println!("--------------------------------");
-        let contents = snow_read::snow_example_long::<&str>(None)?;
-        let item_long: Unload = serde_xml_rs::from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
-        let contents = snow_read::snow_example_search();
-        let item_search: Unload = serde_xml_rs::from_str(&contents).expect("Failed to parse `&contents` into `Unload`");
+
+        // Note: I'm a little surprised a reference works here, as I'd think the String would be immediately dropped.
+        let item_long: Unload = serde_xml_rs::from_str(&fs::read_to_string(SNOW_EXAMPLE_LONG_FILE)?)?;
+        let item_search: Unload = serde_xml_rs::from_str(&fs::read_to_string(SNOW_EXAMPLE_SEARCH_FILE)?)?;
+        trace!(?item_search, "hi....");
 
         let Script::SysScriptInclude(search_contents) = &item_search.scripts[0] else { panic!("nope") };
         for script in &item_long.scripts {
