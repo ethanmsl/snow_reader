f6570db6b4f332a573d169cc47b9c7202bac7027
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-11-28 17:53:16 -0500

    chore: minor changes to roxmltree example

--- examples/roxml_ex.rs
+++ examples/roxml_ex.rs
@@ -1,32 +1,47 @@
 //! roxmltree example getting stats from an XML file
+//!
+//! clear; carrex roxml_ex snow_files/PD\ Business\ rules.xml
 
 use std::collections::HashSet;
 
 fn main()
 {
-        let args: Vec<_> = std::env::args().collect();
+        let args = {
+                let args: Vec<_> = std::env::args().collect();
 
-        if args.len() != 2 {
-                println!("Usage:\n\tcargo run --example stats -- input.xml");
-                std::process::exit(1);
-        }
-
-        let text = std::fs::read_to_string(&args[1]).unwrap();
-        let opt = roxmltree::ParsingOptions { allow_dtd: true, ..roxmltree::ParsingOptions::default() };
-        let doc = match roxmltree::Document::parse_with_options(&text, opt) {
-                Ok(v) => v,
-                Err(e) => {
-                        println!("Error: {}.", e);
+                if args.len() != 2 {
+                        println!("Usage:\n\tcargo run --example stats -- input.xml");
                         std::process::exit(1);
                 }
+                args
         };
 
-        println!("Elements count: {}", doc.root().descendants().filter(|n| n.is_element()).count());
+        let text = std::fs::read_to_string(&args[1]).unwrap();
+        let doc = {
+                let opt = roxmltree::ParsingOptions { allow_dtd: true, ..roxmltree::ParsingOptions::default() };
+                let doc = match roxmltree::Document::parse_with_options(&text, opt) {
+                        Ok(v) => v,
+                        Err(e) => {
+                                println!("Error: {}.", e);
+                                std::process::exit(1);
+                        }
+                };
+                doc
+        };
+
+        // some prints
+        {
+                for (i, node) in doc.root().descendants().enumerate() {
+                        println!("element {i}:\n{:?}\n", node);
+                }
+                println!("\nInput:\n---\n{}\n---\n", doc.input_text());
+                println!("Elements count: {}", doc.root().descendants().filter(|n| n.is_element()).count());
+        }
 
         let attrs_count: usize = doc.root().descendants().map(|n| n.attributes().len()).sum();
-        println!("Attributes count: {}", attrs_count);
-
         let ns_count: usize = doc.root().descendants().map(|n| n.namespaces().len()).sum();
+
+        println!("Attributes count: {}", attrs_count);
         println!("Namespaces count: {}", ns_count);
 
         let mut uris = HashSet::new();
