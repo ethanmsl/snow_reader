5125ad9d59753c0e312512b5428af7031163297c
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-23 18:32:06 -0500

    feat(xp): xml_schema_gen + quickml
    Scalable way to work with reading variable xml files of varying types.

---
+++ src/bin/try_xml_schema_gen_quickxml.rs
@@ -0,0 +1,66 @@
+//! Attempting auto-derivation using
+//!
+//! `xml_schema_generator snow_files/fake_simple.xml --derive 'Serialize, Deserialize, Debug'`
+
+use std::fs;
+
+use quick_xml::de;
+use serde::{Deserialize, Serialize};
+
+const SNOW_FAKE_SIMPLE_FILE: &str = "snow_files/fake_simple.xml";
+
+fn main() -> Result<(), Box<dyn core::error::Error>> {
+        println!("------------------------------------------");
+        println!("reading from file {}", SNOW_FAKE_SIMPLE_FILE);
+        println!("------------------------------------------");
+
+        let simple_xml_string: String = fs::read_to_string(SNOW_FAKE_SIMPLE_FILE)?;
+
+        println!("------------------------------------------");
+        println!("string{}", simple_xml_string);
+        println!("------------------------------------------");
+
+        let test_serd_xml: Unload = de::from_str(&fs::read_to_string(SNOW_FAKE_SIMPLE_FILE)?)?;
+        println!("------------------------------------------");
+        dbg!("{}", test_serd_xml);
+        println!("------------------------------------------");
+
+        // let config: Config = from_str(XML)?;
+        Ok(())
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Unload {
+        #[serde(rename = "@unload_date")]
+        pub unload_date:            String,
+        #[serde(rename = "$text")]
+        pub text:                   Option<String>,
+        pub herring1:               Herring1,
+        pub herring2:               Herring2,
+        pub sysevent_script_action: SyseventScriptAction,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Herring1 {}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Herring2 {}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SyseventScriptAction {
+        #[serde(rename = "@action")]
+        pub action:                 String,
+        #[serde(rename = "$text")]
+        pub text:                   Option<String>,
+        pub active:                 String,
+        pub event_name:             String,
+        pub sys_replace_on_upgrade: String,
+        pub sys_scope:              SysScope,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SysScope {
+        #[serde(rename = "@display_value")]
+        pub display_value: String,
+        #[serde(rename = "$text")]
+        pub text:          Option<String>,
+}
